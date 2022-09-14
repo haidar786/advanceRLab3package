43f8f2991df34a99f65bed1f5323c7d567948730
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-09-14 15:03:27 +0200

    eucidean added

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,1 +1,14 @@
 print("euclidean")
+euclidean <- function(a, b) {
+  one <- a;
+  two <- b;
+
+  while (two != 0) {
+    rem <- one%%two
+    one <- two
+    two <- rem
+  }
+  return(one)
+}
+
+euclidean(20, 8)
