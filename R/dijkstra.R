a6f55692265d24e0cef6d7d8c9fae6a1c5ab946c
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-09-16 21:02:07 +0200

    dijkstra done

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,17 +1,41 @@
 dijkstra <- function(graph, init_node) {
+  is.scalar <- function(s) is.atomic(s) && length(s) == 1L
+  if (!is.data.frame(graph) | !is.numeric(init_node) | !is.scalar(init_node) | !init_node %in% graph[,1]) {
+    stop("Please enter the right data arguments")
+  }
+
+  if (!"v1" %in% colnames(graph) | !"v2" %in% colnames(graph) | !"w" %in% colnames(graph)) {
+    stop("Please enter column names as v1, v2, w respectively")
+  }
+  if (length(graph[,1]) != length(graph[,3]) | length(graph[,2]) != length(graph[,3])) {
+    stop("Please enter the all edges")
+  }
   uniqueNodes <- unique(graph[,1])
   uniqueNodesGraph <- as.data.frame(uniqueNodes)
   for (i in seq(1,length(uniqueNodes))) {
     uniqueNodesGraph$distance[i] <- Inf
-    uniqueNodesGraph$prev[i] <- NA
+    uniqueNodesGraph$prev[i] <- 0
   }
   uniqueNodesGraph[,2][init_node] <- 0
-  while (length(uniqueNodes) > 0) {
 
-    neighbers <- graph[,2][1:3]
-    print(neighbers)
-    stop()
+  while (length(uniqueNodes) > 0) {
+    u <- uniqueNodes[1]
+    neighbours <- graph[,2][which(graph[,1] == u)]
+    uniqueNodes<-uniqueNodes[-1]
+
+    for (neighbour in neighbours){
+      distU <- graph[graph[,1] == u & graph[,2] == neighbour,3]
+      edges <- uniqueNodesGraph[uniqueNodesGraph[,1] == u,2]
+      distNeighbour <- uniqueNodesGraph[uniqueNodesGraph[,1] == neighbour,2]
+      if (distU + edges < distNeighbour) {
+        uniqueNodesGraph[uniqueNodesGraph[,1] == neighbour,2] <- graph[graph[,1] == u & graph[,2]==neighbour,3] +
+          uniqueNodesGraph[uniqueNodesGraph[,1] == u,2]
+
+      }
+    }
+    uniqueNodesGraph[uniqueNodesGraph[,1] == u, 3] <- 1
   }
+  return(uniqueNodesGraph[,2])
 }
 
 wiki_graph <-
